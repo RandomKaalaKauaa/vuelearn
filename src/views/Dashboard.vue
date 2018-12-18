5347af2fe483d9550bcfe7d858f467ddfccd21c5
djooffice <lam.ud2012@gmail.com>
2018-12-18 16:46:34 +0530

    office

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,12 +2,35 @@
 <div class='dashboard'>
   <h1 class="subheading grey--text text--darken-1">Dashboard</h1>
  
-  <v-btn>click me</v-btn>
   <v-btn depressed small dark color="blue" >
    <v-icon left small>email</v-icon> <span>Email me</span>    
-    </v-btn>
+  </v-btn>
+
   <v-container class='my-5'>
-    content
+    <v-card flat class='pa-4'>
+      <v-layout wrap row>
+        <v-flex xs12 md6>
+          <div class="caption grey--text">Project Title</div>
+          <div>todo list new vuetify app</div>
+        </v-flex>
+
+        <v-flex xs6 sm4 md2>
+          <div class="caption grey--text">Person</div>
+          <div>Djo</div>
+        </v-flex>
+
+        <v-flex xs6 sm4 md2>
+          <div class="caption grey--text">Due by</div>
+          <div>30 november 2019</div>
+        </v-flex>
+
+        <v-flex xs6 sm4 md2>
+          <div class="caption grey--text">Status</div>
+          <div>Ongoing</div>
+        </v-flex>
+
+      </v-layout>
+    </v-card>
   </v-container>
 </div>
 </template>
